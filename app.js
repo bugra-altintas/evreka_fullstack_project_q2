229b22d6f821451ffcb7df53c8894cca41aba981
Buğra Altıntaş <78366274+bugra-altintas@users.noreply.github.com>
2021-04-08 16:38:43 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+class Request{
+    constructor(){
+        this.xhr = new XMLHttpRequest();
+    }
+    get(url,callback){
+        this.xhr.open("GET",url);
+
+        this.xhr.onload = function(){
+            if(this.xhr.status == 200){
+                callback(null,this.xhr.responseText);
+            }
+            else{
+                callback("Hata",null);
+            } 
+        }.bind(this);
+        this.xhr.send();
+    }
+}
+
+
+
+document.getElementById("ajax").addEventListener("click",create);
+function create(){
+    console.log("update");
+    const req = new Request();
+    req.get("https://www.bloomberght.com/piyasa/intradaydata/dolar",function(err,response){
+        const responsej = JSON.parse(response);
+        datas = responsej.SeriesData.slice(-10).reverse();
+        table = document.getElementById("data");
+        table.innerHTML = "";
+        datas.forEach(function(data){
+            const date = new Date(data[0]);
+            const dateformat = date.toLocaleString();
+            table.innerHTML += `
+            <tr>
+                <td>${dateformat}</td>
+                <td>${data[1]}</td>
+            </tr>
+        `
+        });
+    });
+}
+create();
